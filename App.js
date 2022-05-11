115fb08edeb26ff6a3c625923aa32fa42f1b854a
JesperHagman <jesper.hagman98@gmail.com>
2022-05-11 11:31:50 +0200

    setup

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { DataTable } from 'react-native-paper';
+import { useEffect } from 'react';
+
+const todos = [
+  "Städa",
+  "Diska",
+  "Plugga",
+  "Träna"
+]
+
+const handleTodos = () =>{
+
+  for (var i of todos) {
+      
+    }
+    /* 
+  <DataTable.Row>
+    <DataTable.Cell>{i}</DataTable.Cell>
+    <DataTable.Cell> </DataTable.Cell>
+    <DataTable.Cell> </DataTable.Cell>
+  </DataTable.Row> */
+}
+
 
 export default function App() {
+  useEffect(
+    handleTodos
+  )
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>To do</DataTable.Title>
+          <DataTable.Title>Doing</DataTable.Title>
+          <DataTable.Title>Done</DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row>
+          <DataTable.Cell>{todos}</DataTable.Cell>
+          <DataTable.Cell>Diska</DataTable.Cell>
+          <DataTable.Cell>Plugga som fan</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>{todos}</DataTable.Cell>
+        </DataTable.Row>
+
+      </DataTable>
+      
     </View>
   );
 }
